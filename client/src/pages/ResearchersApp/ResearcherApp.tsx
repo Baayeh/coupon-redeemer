5b60e33c50b3670d23e542e4c0dc80dafd5c2f9f
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-26 18:57:40 +0000

    Create component to generate coupons

---
+++ client/src/pages/ResearchersApp/ResearcherApp.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export const ResearcherApp = () => {
+  return (
+    <section>
+      <h2>ResearcherApp</h2>
+      <Link to='/'>Back to home</Link>
+    </section>
+  )
+}
---
+++ client/src/pages/ResearchersApp/index.ts
@@ -0,0 +1,2 @@
+export { ResearcherApp } from "./ResearcherApp";
+
