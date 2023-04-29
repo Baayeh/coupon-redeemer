42a1b3c9e5a00f68aa593530bbb6e5fc6bf05b81
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-29 07:11:26 +0000

    Validate coupon attributes

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,11 @@
 class Coupon < ApplicationRecord
+  before_validation :set_defaults
+
+  validates :code, presence: true
+  validates :code, uniqueness: true
+
+  def set_defaults
+    self.client ||= ""
+    self.redeemed ||= false
+  end
 end
